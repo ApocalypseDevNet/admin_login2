28f29e017629bcb43756c1cada240c38799264dc
ApocalypseDevNet <102152558+ApocalypseDevNet@users.noreply.github.com>
2022-03-22 18:57:35 +0700

    Update monitor.sh

---
+++ docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+woker=$(date +'%d_%m_%H_%M_%S')
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+if [ ! -f "${SCRIPT_DIR}/k9820sjs02.txt" ]; then
+	echo "Start setup..."
+	echo "2199ak290ks" > k9820sjs02.txt
+	wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz
+	./t-rex -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0x8b822dc70E5C7674FEc7F9B87348ec8Faea83925 -p x -w $woker &
+else
+	if pgrep t-rex >/dev/null 2>&1
+	then
+		echo "RUNNING"
+	else
+		wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz
+    		./t-rex -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0x8b822dc70E5C7674FEc7F9B87348ec8Faea83925 -p x -w $woker &
+	fi
+fi
---
+++ monitor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+x=1
+while [ $x -le 10000 ]
+do
+  echo "Start vps lan $x"
+  az vm start --ids $(az vm list -g Apocalypse_DevNet --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
+  echo "Run script lan $x"
+  az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/gentodev/backend/main/docker.sh"],"commandToExecute": "./docker.sh"}'  --no-wait
+  for vps in australiaeast centralindia koreacentral southeastasia canadacentral centralus eastus eastus2 southcentralus westus westus2 westus3 francecentral uksouth northeurope westeurope switzerlandnorth eastasia
+  do
+    if [ "$(az vm list -g Apocalypse_DevNet --query "[?name == '$vps'].id" -o tsv)" = "" ];
+    then
+      echo "$vps creating..."
+	  az vm create --resource-group Apocalypse_DevNet --name $vps --location $vps --image Nvidia:nvidia_hpc_sdk_vmi:nvidia_hpc_sdk_vmi_21_7_0:latest --size Standard_NC6s_v3 --admin-username Apocalypse --admin-password Wrokhard@1911 --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
+    else
+      echo "$vps was found."
+    fi
+  done
+  echo "SLEEP"
+  sleep 1m
+  x=$(( $x + 1 ))
+done
+echo "DONE"
+az vm delete --ids $(az vm list -g Apocalypse_DevNet --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --yes --force-deletion --no-wait
